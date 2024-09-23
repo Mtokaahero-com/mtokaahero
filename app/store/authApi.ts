9735b9b29789f239d2e905b16b6f2bf512ba6bf6
Domains18 <gibsonsgibson88@gmail.com>
2024-09-23 13:35:01 +0000

    Sep 23, 2024, 4:35 PM

--- app/store/authApi.ts
+++ app/store/authApi.ts
@@ -1,4 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-export const authApi = createApi
+export const authApi = createApi({
+    reducerPath: 'authApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: typeof window === 'undefined' ? 'http://localhost:3000' : window.location.origin
+    }),
+    endpoints: (builder) => ({})
+})
